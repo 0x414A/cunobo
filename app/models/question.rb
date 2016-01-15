e55df73578fc222a3c5ac25c74c6f1e411447178
Allyson Julian <aj@hckr.eml.cc>
2016-01-14 21:36:14 -0800

    Be able to assign a question to a student

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,7 @@ class Question < ActiveRecord::Base
   has_many :student_questions
 
   def assign!(students)
-    students.ids.each do |student_id|
+    students.map(&:id).each do |student_id|
       StudentQuestion.create(student_id: student_id, question_id: self.id)
     end
   end
