818bef73a6d44111b7a129f441cc489311a3ce85
Allyson Julian <aj@hckr.eml.cc>
2016-01-15 18:52:00 -0800

    Wrap in a container

--- app/assets/javascripts/components/app.vue
+++ app/assets/javascripts/components/app.vue
@@ -1,13 +1,19 @@
 <template>
-  <title-bar></title-bar>
-  <router-view></router-view>
+  <div class="main">
+    <title-bar></title-bar>
+    <div class="container">
+      <router-view></router-view>
+    </div>
+  </div>
 </template>
 
 <script lang="coffee">
   TitleBar = require './title_bar.vue'
-  
+  Question = require './question.vue'
+
   module.exports =
     components:
       'title-bar': TitleBar
+      'question': Question
 </script>
 
--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,15 +1,17 @@
 //= require_self
 
-// CommonJS
+// Vue modules
 var Vue = require('vue')
 var Router = require('vue-router')
 var Resource = require('vue-resource')
-var App = require('./components/app.vue')
-var Dashboard = require('./components/dashboard.vue')
 
 // Register modules
 Vue.use(Router)
 Vue.use(Resource)
+
+// Components
+var App = require('./components/app.vue')
+var Dashboard = require('./components/dashboard.vue')
 
 // Set routing
 var router = new Router()
