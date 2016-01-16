a6f4a2fff03e521d705c19aa0b417211c2bb2711
Allyson Julian <aj@hckr.eml.cc>
2016-01-15 19:33:02 -0800

    Add editor view

---
+++ app/assets/javascripts/components/editor.vue
@@ -0,0 +1,6 @@
+<template>
+  <form>
+    <textarea class="u-full-width" placeholder="Type your thoughts" id="entry-writing"></textarea>
+    <input class="button-primary" type="submit" value="Submit">
+  </form>
+</template>
--- app/assets/javascripts/components/home.vue
+++ app/assets/javascripts/components/home.vue
@@ -1,11 +1,14 @@
 <template>
   <question></question>
+  <editor></editor>
 </template>
 
 <script lang="coffee">
   Question = require './question.vue'
+  Editor = require './editor.vue'
 
   module.exports =
     components:
       'question': Question
+      'editor': Editor
 </script>
