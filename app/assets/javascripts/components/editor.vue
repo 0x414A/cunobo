bbda98fbb71a3aaa96e927abe2d42775817f0ed2
Allyson Julian <aj@hckr.eml.cc>
2016-01-15 19:38:45 -0800

    Tap to answer

--- app/assets/javascripts/components/editor.vue
+++ app/assets/javascripts/components/editor.vue
@@ -1,6 +1,16 @@
 <template>
-  <form>
+  <p v-if="!shouldShowEditor" v-on:click="showEditor">Tap to answer</p>
+  <form v-if="shouldShowEditor">
     <textarea class="u-full-width" placeholder="Type your thoughts" id="entry-writing"></textarea>
     <input class="button-primary" type="submit" value="Submit">
   </form>
 </template>
+
+<script lang="coffee">
+  module.exports =
+    data: ->
+      shouldShowEditor: false
+    methods: 
+      showEditor: ->
+        @shouldShowEditor = true
+</script>
