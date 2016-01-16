a92a34a35f21d224cc046de7ee8ff1e7887cf559
Allyson Julian <aj@hckr.eml.cc>
2016-01-15 19:26:16 -0800

    Set default view

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -11,15 +11,25 @@ Vue.use(Resource)
 
 // Components
 var App = require('./components/app.vue')
+
+// Views
+var Question = require('./components/question.vue')
 var Dashboard = require('./components/dashboard.vue')
 
 // Set routing
 var router = new Router()
 
 router.map({
+  '/home': {
+    component: Question
+  },
   '/dashboard': {
     component: Dashboard
   }
+})
+
+router.redirect({
+   '*': '/home'
 })
 
 router.start(App, "#app")
