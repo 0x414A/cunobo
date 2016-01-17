1e049c747115c4d57a6f390141169e3d0ca298a8
Allyson Julian <aj@hckr.eml.cc>
2016-01-17 12:27:24 -0800

    Add another simple context block as an example

--- spec/models/student_question_spec.rb
+++ spec/models/student_question_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe StudentQuestion, type: :model do
-  let(:question){FactoryGirl.create(:question)}
+  let(:question_text){Faker::Lorem.sentence}
+  let(:question){FactoryGirl.create(:question, text: question_text)}
   let(:student){FactoryGirl.create(:student)}
   let(:student_question){
     FactoryGirl.create(:student_question, 
@@ -12,4 +13,11 @@ RSpec.describe StudentQuestion, type: :model do
   it "belongs to a question" do
     expect(student_question.question).to eq(question)
   end
+
+  context 'text' do
+    it "returns the parent question text" do
+      expect(student_question.text).to eq(question_text)
+    end
+  end
 end
+
